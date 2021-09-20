e49973155d0f108e05e3e4096151b55d2c76cffa
Jonathan Easterman <jonathaneasterman@gmail.com>
2021-09-20 14:48:51 +0100

    Specify create-wallets subcommand using yargs

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,3 @@
-import * as yargs from 'yargs';
 
 class Config {
   numWallets: number
@@ -7,29 +6,6 @@ class Config {
   constructor(numWallets: number, walletsDir: string) {
     this.numWallets = numWallets
     this.walletsDir = walletsDir
-  }
-
-  public static parseArgs() {
-    const argv = yargs.options({
-      numWallets: {
-        alias: 'n',
-        type: 'number',
-        demand: true,
-        description: 'Number of Wallets to create or load'
-      },
-      walletsDir: {
-        alias: 'w',
-        type: 'string',
-        default: './wallets',
-        description: 'Directory to save wallets'
-      }
-    })
-    .check(data => {
-      return !isNaN(data.numWallets);
-    })
-    .parseSync();
-
-    return new Config(argv.numWallets, argv.walletsDir)
   }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,40 @@
 #!/usr/bin/env node
 
 import fs = require('fs')
+import * as yargs from 'yargs'
+import { help } from 'yargs'
 import Config from './config'
 import { WalletGenerator, Keypair } from './wallet'
 
 function main() {
-  const config = Config.parseArgs()
+  const argv = yargs
+    .command(
+      'create-wallets <n>',
+      'create n wallets',
+      yargsBuilder =>
+        yargsBuilder
+          .positional('n', {
+            describe: 'Number of Wallets to create or load',
+            type: 'number',
+            demandOption: true,
+          })
+          .option('walletsDir', {
+            alias: 'w',
+            type: 'string',
+            default: './wallets',
+            description: 'Directory to save wallets'
+          }),
+      (args) => createWallets(new Config(args.n, args.walletsDir))
+    )
+    .help('help').wrap(null).argv
+    console.log(argv)
+}
+
+function createWallets(config: Config) {
+  if (config.numWallets < 1) {
+    console.error('Must specify positive number of wallets')
+    process.exit(1)
+  }
   console.log(`Creating ${config.numWallets} wallets`)
 
   const wallets = WalletGenerator.wallets(config.numWallets)
@@ -14,6 +43,7 @@ function main() {
     new Keypair(wallet.address, wallet.privateKey)
       .saveToDisk(savePath)
   })
+
 }
 
 main()
