c04a76e2b1d9a6de500046a9c58bab2945292c12
Jonathan Easterman <jonathaneasterman@gmail.com>
2021-09-20 16:24:05 +0100

    Add environment variable parsing for mnemonic and node rpc

--- Readme.md
+++ Readme.md
@@ -9,3 +9,9 @@ Creates n wallets
 
 Funds wallets with n ETH
 `npx ts-node src/index.ts fund-wallets 1`
+
+## Environment Variables
+Create a `.env` file based on the `.env.example` included in the repo.
+You need to fill in the mnemonic of the wallet used to fund your new, airdrop-elligible wallets.
+
+You also need to fill in the URL of an RPC node. You can grab a personal RPC for free by creating an account with [Infura](https://infura.io/) or [Moralis](https://moralis.io/)
--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,37 @@
-interface Config {
+import * as dotenv from 'dotenv';
+
+interface Arguments {
   amount?: number
   numWallets?: number
   walletsDir: string
+}
+
+class Config {
+  amount?: number
+  numWallets?: number
+  walletsDir: string
+  mnemonic?: string
+  nodeUrl?: string
+
+  constructor(argv: Arguments) {
+    this.numWallets = argv.numWallets
+    this.walletsDir = argv.walletsDir
+    this.amount = argv.amount
+  }
+
+  public parseEnvVars() {
+    const configResult = dotenv.config();
+
+    if (configResult.error) {
+      throw configResult.error;
+    }
+
+    this.mnemonic = process.env.MNEMONIC || '';
+    this.nodeUrl = process.env.NODE_URL || '';
+    if (this.mnemonic == '' || this.nodeUrl == '') {
+      throw Error('Set MNEMONIC and/or NODE_URL env variables')
+    }
+  }
 }
 
 export default Config
